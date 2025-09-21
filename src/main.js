dbba4e2baaf54b6d0064a972a7903f2f5fad67fa
Mcarby <sifddin@hotmail.com>
2025-09-21 22:21:35 +0200

    chore: clean unused code

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
+
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 import './../src/helper/fun_helper.js'
@@ -10,7 +10,7 @@ document.querySelector('#app').innerHTML = `
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+   
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
