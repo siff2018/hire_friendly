83ede42c6014f7519b2ee91983f2be43db1c6283
Mcarby <sifddin@hotmail.com>
2025-09-21 21:55:48 +0200

    fix: add default in parameter

--- src/helper/fun_helper.js
+++ src/helper/fun_helper.js
@@ -2,7 +2,7 @@
 I hereby  I create production code for these productive functions
 */
 
-export function double(arr){
+export function double(arr = []){
    if(!Array.isArray(arr)){
 
        throw new Error("You have not passed Array!")   
