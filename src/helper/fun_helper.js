b84d05fec166bf820cab33e8d44b89a48bbffcf8
Mcarby <sifddin@hotmail.com>
2025-09-21 17:57:06 +0200

    refact: simplify double() function

--- src/helper/fun_helper.js
+++ src/helper/fun_helper.js
@@ -3,15 +3,13 @@ I hereby  I create production code for these productive functions
 */
 
 export function double(arr){
-   if(Array.isArray(arr)){
+   if(!Array.isArray(arr)){
 
-       return arr.map(num => num * 2)
+       throw new Error("You have not passed Array!")   
 
-
-   }else{
-    throw new Error("You have not passed Array!")
-   }
-
+    }
+    
+    return arr.map(num => num * 2)
 }
 
 
